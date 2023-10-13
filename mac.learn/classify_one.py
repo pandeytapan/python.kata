9ba72e519ed0de942f4f38a0ef08067773210c5b
Tapan Pandey <pandey.tapan@hotmail.com>
2023-10-13 19:59:06 +0530

    Tried decision tree classifier to get correct fruit.

--- mac.learn/classify_one.py
+++ mac.learn/classify_one.py
@@ -5,3 +5,22 @@ data = [[120, 80], [100, 60], [130, 90], [150, 100]]
 
 # Example label : Type oof fruit. Apple or Orange
 labels = ['Apple', 'Apple', 'Orange', 'Orange']
+
+# Create a model
+model = DecisionTreeClassifier()
+# train the model over the data
+model.fit(data, labels)
+
+# Predict the type of fruit for given size and weight
+# Do this repeatedly for different values of size and weight
+while True:
+    size = int(input("Enter size of fruit (in cms): "))
+    weight = int(input("Enter weight of fruit (in gms): "))
+    # Model accepts a list of lists as input
+    fruit = model.predict([[size, weight]])
+    print(f"The predicted fruit is {fruit}")
+
+    # Ask user if he wants to continue
+    choice = input("Do you want to continue? (y/n): ")
+    if choice == 'n':
+        break
